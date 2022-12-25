cc9e8e21c02e43994e5cef0d7b90c0f9b27884f7
CarlosChNa <carlos.chover1@hotmail.com>
2022-12-25 13:22:05 +0100

    ejercicio2 finalizado

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,11 @@
+<form method="POST" action="/contacts">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Name</label>
+    <input name="name" type="text" class="form-control" id="name" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Phone number</label>
+    <input type="text" name="phone number" class="form-control" id="phone number">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response as HttpResponse;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
+use LDAP\Result;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,4 +30,68 @@ Route::get('/ejercicio1', function () {
 
 Route::post('/ejercicio1', function () {
     return "POST OK";
+});
+
+
+Route::get('/contacts', function () {
+    return Response::view('/contacts');
+});
+
+Route::post('/contacts', function (Request $request) {
+    return Response::json(["message" => "hola"]);
+});
+
+Route::post('/ejercicio2/a', function (Request $request) {
+    return Response::json($request);
+});
+
+Route::post('/ejercicio2/b', function (Request $request) {
+    if (($request->get('price')) < 0) {
+        return Response::json(["message" => "Price can't be less than 0"])->setStatusCode(422);
+    }
+    else
+    return Response::json($request);
+});
+
+Route::post('/ejercicio2/c', function (Request $request) {
+    if ($request->query('discount') == "SAVE5") {
+        $price = $request->get('price') * 0.95;
+        $discount = 5;
+        return Response::json([
+            'name' => $request->get('name'),
+            'description' => $request->get('description'),
+            'price' => $price,
+            'discount' => $discount
+        ]);
+    }
+    if ($request->query('discount') == "SAVE10") {
+        $price = $request->get('price') * 0.90;
+        $discount = 10;
+        return Response::json([
+            'name' => $request->get('name'),
+            'description' => $request->get('description'),
+            'price' => $price,
+            'discount' => $discount
+        ]);
+    }
+    if ($request->query('discount') == "SAVE15") {
+        $price = $request->get('price') * 0.85;
+        $discount = 15;
+        return Response::json([
+            'name' => $request->get('name'),
+            'description' => $request->get('description'),
+            'price' => $price,
+            'discount' => $discount
+        ]);
+    }
+    else {
+        $discount = 0;
+        return Response::json([
+            'name' => $request->get('name'),
+            'description' => $request->get('description'),
+            'price' => $request->get('price'),
+            'discount' => $discount
+        ]);
+    }
+    
 });
